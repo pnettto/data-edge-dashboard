05b81e60028bcde57a4450f198fa0ef584710be6
Pedro Netto <pedro@pnetto.com>
2025-10-08 13:02:13 +0200

    Remove unused example

--- exploratory_analysis/guillermo/output/config.py
+++ exploratory_analysis/guillermo/output/config.py
@@ -6,31 +6,6 @@ config = [
     {
         'tab': 'Sample charts',
         'items': [
-            {
-                'columns': [
-                    {
-                        'type': 'table',
-                        'title': 'Sample table',
-                        'description': 'Monthly revenue per segment totals as table',
-                        'df': csv_to_df('df_person_supply_agg'),
-                    },
-                    {
-                        'type': 'line',
-                        'title': 'Guillermo example',
-                        'description': 'Monthly revenue tracked by invoice date',
-                        'df': csv_to_df('df_person_supply_agg'),
-                        'x_field': 'month',
-                        'x_label': 'Month',
-                        'y_field': 'recorded_supply_hours',
-                        'y_label': 'Logged hours',
-                        'category_field': 'person_seniority',
-                        'category_label': 'Seniority',
-                        #'forecast': True
-                    },
-                   
-                ]
-            },
-
              {
                 'type': 'line',
                 'title': 'Workload vs. Capacity Over Time',
